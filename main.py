590727c3f6f5119b4e13950fc1fd50d62ab58794
mikehawk1021 <45913084+mikehawk1021@users.noreply.github.com>
2018-12-16 10:53:43 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+favorite song Meta-Data
+"""
+#Global Variables are been declared below
+
+
+Genre = "Blues"
+Rating = 4.5
+Title = "Hello"
+Artist = "Adele"
+DurationInMins = 4 #duration of song in minutes 
+DurationInSecs = 30
+Track = "01"
+ReleaseYear = 2015  #Year Song Was Released 
+Album = "Single"
+
+#printing output
+
+print(Genre)
+print(Rating)
+print(Title)
+print(Artist)
+print(DurationInMins)
+print(DurationInSecs)
+print(Track)
+print(ReleaseYear)
+print(Album)
