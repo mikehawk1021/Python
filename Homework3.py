56d0d3548e136d7bc5775283b6dfcdd2d2d8aac5
mikehawk1021 <45913084+mikehawk1021@users.noreply.github.com>
2018-12-19 20:51:23 +0100

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 19 20:49:40 2018
+
+@author: HP
+"""
+
+
+def aLarm():
+    time = "Day"
+    sleepy = True
+    Venue = "Work"
+    if time == "Day" and sleepy == False:
+        Pajamas = "Off"
+        print(Pajamas)
+    else:
+        Pajamas = "On"
+        print(Pajamas)
+    
+    
+aLarm() 
+    
